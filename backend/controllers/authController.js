fce2e84e13aa01ac26d7fc1a14f8d569f8775202
OshiSenevirathna <oshisenevirathna09@gmail.com>
2025-07-12 12:34:05 +0530

    completed backend

---
+++ backend/controllers/authController.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+// @desc    Register user
+exports.registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ username, email, password });
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error'});
+    console.log("error is ",err)
+  }
+};
+
+// @desc    Login user
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// @desc    Get profile
+exports.getProfile = async (req, res) => {
+  const user = await User.findById(req.user.id).select('-password');
+  res.json(user);
+};
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const authRoutes = require('./routes/authRoutes.js')
+const taskRoutes = require('./routes/taskRoutes.js')
 
 app.use(express.json());
 
 const PORT = process.env.PORT
 const MONGODB_URL = process.env.MONGODB_URL
+
+app.use('/api/auth',authRoutes);
+app.use('/api/task',taskRoutes)
 
 async function mongooseConnect() {
   try {
